e3fe3d8f8e4bf4cbf6c91f4b9c6526a2091f710d
furkanmavili <furkanmavili@outlook.com>
2021-11-26 21:00:11 +0300

    get cells from localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import Layout from "./components/Layout";
 import Toolbar from "./components/Toolbar";
 import Grid from "./components/Grid";
 import Settings from "./components/Settings";
+import { useEffect } from "react";
+import useStore from "./store/useStore";
 
 function App() {
+  const setCells = useStore(state => state.setCells)
+  
+  useEffect(() => {
+    const getItem = localStorage.getItem('grid') 
+    if (!getItem) return;
+    setCells(JSON.parse(getItem))
+  }, [])
 
   return (
     <Layout>
