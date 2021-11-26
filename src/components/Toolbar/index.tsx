94bf15e7db801d687a0b76dff5d0e55044478af1
furkanmavili <furkanmavili@outlook.com>
2021-11-26 21:01:46 +0300

    add modal to save option

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,24 +1,31 @@
+import { useState } from "react";
 import useStore from "../../store/useStore";
 import Palette from "../Palette";
+import Preview from "../Preview";
 import Tools from "./Tools";
 
 function Toolbar() {
   const resetCells = useStore((state) => state.resetCells);
-
+  const [showPreview, setShowPreview] = useState(false);
   return (
-    <div className="flex flex-col space-y-10  mb-10 md:mb-0">
-      <div>Toolbar</div>
-      <div className="flex justify-between md:justify-start md:flex-col md:space-y-10">
-        <div className="flex flex-col md:flex-row">
-          <button type="button" className="nes-btn is-success flex-1">Save</button>
-          <button onClick={resetCells} type="button" className="nes-btn flex-1">
-            Reset
-          </button>
+    <>
+      <div className="flex flex-col space-y-10  mb-10 md:mb-0">
+        <div>Toolbar</div>
+        <div className="flex justify-between md:justify-start md:flex-col md:space-y-10">
+          <div className="flex flex-col md:flex-row">
+            <button onClick={() => setShowPreview(true)} type="button" className="nes-btn is-success flex-1">
+              Save
+            </button>
+            <button onClick={resetCells} type="button" className="nes-btn flex-1">
+              Reset
+            </button>
+          </div>
+          <Tools />
         </div>
-      <Tools />
-      </div>
         <Palette />
-    </div>
+      </div>
+      <Preview isOpen={showPreview} closeModal={() => setShowPreview(false)} />
+    </>
   );
 }
 
